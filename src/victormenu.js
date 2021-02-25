d90a3a1e6912cccfbce6e391248c06ad3c48e56c
victor1591 <78681754+victor1591@users.noreply.github.com>
2021-02-25 18:26:05 -0300

    Update victormenu.js

--- src/victormenu.js
+++ src/victormenu.js
@@ -1,1 +1,15 @@
 
+const victormenu = (prefix, pushname) => {
+    return `◪ *Comandos do Victor*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}clonr
+    ├─ ❏ ${prefix}
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.nabutomenu = nabutomenu
