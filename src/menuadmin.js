99509943237acd8c837a82cf16b7218745982c34
victor1591 <78681754+victor1591@users.noreply.github.com>
2021-02-25 18:37:29 -0300

    Update menuadmin.js

--- src/menuadmin.js
+++ src/menuadmin.js
@@ -1,31 +1,26 @@
 const menuadmin = (prefix, pushname) => {
- return `oiin aqui e o menu dos admins
- _obs para o bot executar esses comandos e nescessario dar adm para o bot_
- 
- ◪ *COMANDO DOS ADMINS*
+ return `◪ COMANDOS DOS ADMINS ⛧᭄
  │
- ├─ ❏ ${prefix}opengc
- ├─ ❏ ${prefix}closegc
- ├─ ❏ ${prefix}promote
- ├─ ❏ ${prefix}demote
- ├─ ❏ ${prefix}tagall
- ├─ ❏ ${prefix}tagall2
- ├─ ❏ ${prefix}tagall3
- ├─ ❏ ${prefix}tagall4
- ├─ ❏ ${prefix}tagall5
- ├─ ❏ ${prefix}add
- ├─ ❏ ${prefix}kick
- ├─ ❏ ${prefix}listadmins
- ├─ ❏ ${prefix}linkgroup
- ├─ ❏ ${prefix}leave
- ├─ ❏ ${prefix}welcome
- ├─ ❏ ${prefix}nsfw
- ├─ ❏ ${prefix}leveling
- ├─ ❏ ${prefix}level
- ├─ ❏ ${prefix}delete
- ├─ ❏ ${prefix}simih
- └─ ❏ ${prefix}ownergroup
- `
+ ├─ ❏ ${prefix}opengc       ☆abrir grupo 
+ ├─ ❏ ${prefix}closegc      ☆fechar grupo 
+ ├─ ❏ ${prefix}promote      ☆promover para adm
+ ├─ ❏ ${prefix}demote       ☆remover como adm
+ ├─ ❏ ${prefix}tagall       ☆marcar todos do grupo 
+ ├─ ❏ ${prefix}tagall2      ☆marcar todos do grupo
+ ├─ ❏ ${prefix}tagall3      ☆marcar todos do grupo
+ ├─ ❏ ${prefix}tagall4      ☆marcar todos do grupo
+ ├─ ❏ ${prefix}tagall5      ☆marcar todos do grupo
+ ├─ ❏ ${prefix}add               ☆add membro
+ ├─ ❏ ${prefix}kick              ☆remover membro
+ ├─ ❏ ${prefix}listadmins        ☆marcar os adms
+ ├─ ❏ ${prefix}linkgroup         ☆link do grupo 
+ ├─ ❏ ${prefix}leave             ☆tirar o bot 
+ ├─ ❏ ${prefix}welcome[0/1]      ☆boas vindas 
+ ├─ ❏ ${prefix}nsfw              ☆+18
+ ├─ ❏ ${prefix}leveling[0/1]     ☆colocar níveis 
+ ├─ ❏ ${prefix}level        ☆ver seu nível
+ ├─ ❏ ${prefix}del          ☆fazer o BOT apagar a mensagem
+ └─ ❏ ${prefix}ownergroup      ☆dono do grupo`
 
 
 }
